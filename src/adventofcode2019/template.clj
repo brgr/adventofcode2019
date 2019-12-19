7629370ea368f0932376a543a004bcbae33934bd
Dominik Berger <dmnkbrgr@gmail.com>
2019-12-19 19:35:28 +0100

    add a general template file

---
+++ src/adventofcode2019/template.clj
@@ -0,0 +1,13 @@
+(ns adventofcode2019.template
+  (:require [clojure.string :as str]
+            [clojure.core.match :refer [match]]
+            [clojure.math.numeric-tower :as math]
+            [clojure.set :as set])
+  (:use [adventofcode2019.core]))
+
+
+
+(defn -main []
+  (let [input (slurp "resources/day.input")]
+    (do
+      (println "TODO"))))
