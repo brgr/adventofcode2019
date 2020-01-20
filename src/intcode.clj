1d199a3996669b3860937fd399b2c539080cd2cc
Dominik Berger <dmnkbrgr@gmail.com>
2020-01-20 23:08:19 +0100

    #day11 fix intcode: parameter mode for 3rd parameter was not taken into account

--- src/intcode.clj
+++ src/intcode.clj
@@ -11,8 +11,8 @@
     (if (nil? result) 0 result)))
 
 ; parameter_position starting at 0 for first parameter
-(defn- parameter_mode [instruction parameter_position]
-  (nth (instruction :param_modes) parameter_position))
+(defn- parameter_mode [instruction parameter-position]
+  (nth (instruction :param-modes) parameter-position))
 
 ; parameter_position in relation to opcode_position
 (defn- parameter [intcode instruction opcode_position relative-base parameter_position]
@@ -42,100 +42,102 @@
     1 :immediate
     2 :relative))
 
-(defn- destructure_instruction [instruction]
+(defn- destructure-instruction [instruction]
   (let [opcode (mod instruction 100)
-        param1_mode (transform_param_mode (quot (mod instruction 1000) 100))
-        param2_mode (transform_param_mode (quot (mod instruction 10000) 1000))]
+        param1-mode (transform_param_mode (quot (mod instruction 1000) 100))
+        param2-mode (transform_param_mode (quot (mod instruction 10000) 1000))
+        param3-mode (transform_param_mode (quot (mod instruction 100000) 10000))]
     {:opcode      opcode
-     :param_modes [param1_mode param2_mode]}))
+     :param-modes [param1-mode param2-mode param3-mode]}))
 
-(defn- process_single [{:keys [intcode position relative-base input output]}]
-  (let [instruction (destructure_instruction (geti intcode position))
+(defn- process-single [{:keys [intcode position relative-base input output] :as machine}]
+  (let [instruction (destructure-instruction (geti intcode position))
         first_parameter (parameter intcode instruction position relative-base 1)
         second_parameter (parameter intcode instruction position relative-base 2)
         third_parameter (third_parameter intcode instruction position relative-base 3)]
 
     (case (instruction :opcode)
       ;; 1 -- ADD -- Add 1st+2nd param and put result to 3rd
-      1 {:position (+ position 4)
+      1 {:position      (+ position 4)
          :relative-base relative-base
-         :intcode (assoc intcode third_parameter (+ first_parameter second_parameter))
-         :input    input
-         :output   output
-         :state    :running}
+         :intcode       (assoc intcode third_parameter (+ first_parameter second_parameter))
+         :input         input
+         :output        output
+         :state         :running}
 
       ;; 2 -- MUL -- Multiply 1st+2nd param and put result to 3rd
-      2 {:position (+ position 4)
+      2 {:position      (+ position 4)
          :relative-base relative-base
-         :intcode (assoc intcode third_parameter (* first_parameter second_parameter))
-         :input    input
-         :output   output
-         :state    :running}
+         :intcode       (assoc intcode third_parameter (* first_parameter second_parameter))
+         :input         input
+         :output        output
+         :state         :running}
 
-      ; 3 -- Input -- Write the next given input in (the *address* specified in) the 1st parameter
-      ; Also, it seems that '3'/input *always* takes the first parameter as an address
-      ; However, in D9 that seems not to be the case anymore, but I don't know how exactly it is meant
-      3 {:position (+ position 2)
-         :relative-base relative-base
-         :intcode  (assoc intcode third_parameter (first input))
-         :input    (rest input)
-         :output   output
-         :state    :running}
+      ; 3 -- Input -- Write the next given input in the 1st parameter
+      ; Blocks if currently no input is given
+      3 (if (empty? input)
+          (assoc machine :state :halting)
+          {:position      (+ position 2)
+           :relative-base relative-base
+           :intcode       (assoc intcode third_parameter (first input))
+           :input         (rest input)
+           :output        output
+           :state         :running})
 
       ; 4 -- Output --
-      4 {:position (+ position 2)
+      4 {:position      (+ position 2)
          :relative-base relative-base
-         :intcode  intcode
-         :input    input
-         :output   (conj output first_parameter)
-         :state    :running}
+         :intcode       intcode
+         :input         input
+         :output        (conj output first_parameter)
+         :state         :running}
 
-      5 {:position (if (= first_parameter 0) (+ position 3) second_parameter)
+      5 {:position      (if (= first_parameter 0) (+ position 3) second_parameter)
          :relative-base relative-base
-         :intcode  intcode
-         :input    input
-         :output   output
-         :state    :running}
+         :intcode       intcode
+         :input         input
+         :output        output
+         :state         :running}
 
       ;; 6 -- JUMP IF -- If 1st param = 1, jump to 2nd param
-      6 {:position (if (not= first_parameter 0) (+ position 3) second_parameter)
+      6 {:position      (if (not= first_parameter 0) (+ position 3) second_parameter)
          :relative-base relative-base
-         :intcode  intcode
-         :input    input
-         :output   output
-         :state    :running}
-      7 {:position (+ position 4)
+         :intcode       intcode
+         :input         input
+         :output        output
+         :state         :running}
+      7 {:position      (+ position 4)
          :relative-base relative-base
-         :intcode  (if (< first_parameter second_parameter)
-                     (assoc intcode third_parameter 1)
-                     (assoc intcode third_parameter 0))
-         :input    input
-         :output   output
-         :state    :running}
+         :intcode       (if (< first_parameter second_parameter)
+                          (assoc intcode third_parameter 1)
+                          (assoc intcode third_parameter 0))
+         :input         input
+         :output        output
+         :state         :running}
 
       ;; 8 -- EQUALS -- If (1st = 2nd) , write 1 to 3rd, else 0 to 3rd
-      8 {:position (+ position 4)
+      8 {:position      (+ position 4)
          :relative-base relative-base
-         :intcode  (if (= first_parameter second_parameter)
-                     (assoc intcode third_parameter 1)
-                     (assoc intcode third_parameter 0))
-         :input    input
-         :output   output
-         :state    :running}
+         :intcode       (if (= first_parameter second_parameter)
+                          (assoc intcode third_parameter 1)
+                          (assoc intcode third_parameter 0))
+         :input         input
+         :output        output
+         :state         :running}
 
       ; 9 -- Adjust relative base using its only parameter
-      9 {:position (+ position 2)
+      9 {:position      (+ position 2)
          :relative-base (+ relative-base first_parameter)
-         :intcode  intcode
-         :input    input
-         :output   output
-         :state    :running}
+         :intcode       intcode
+         :input         input
+         :output        output
+         :state         :running}
       )))
 
 (defn run-machine [{:keys [position intcode] :as machine}]
   (if (= stop_instruction (geti intcode position))
     (assoc machine :state :stopped)
-    (let [machine (process_single machine)]
+    (let [machine (process-single machine)]
       (if (= (machine :state) :halting)
         machine
         (recur machine)))))
