d991099e93be78b1c7507fc5be9880262f38c0ff
Dominik Berger <dmnkbrgr@gmail.com>
2020-01-29 19:23:15 +0100

    #day12 add finished part 1 and start with part 2 (inside part 1)
    Note: Part 2 is, in general, already finished as well but has a very long runtime - too long to be feasible (I did not
    find the result). Thus, a better algorithm needs to be used -> LCM = Least Common Multiple! (kgV)

--- src/adventofcode2019/day12part1.clj
+++ src/adventofcode2019/day12part1.clj
@@ -11,6 +11,12 @@
    {:x -6, :y 14, :z -4}
    {:x 4, :y -4, :z -3}])
 
+(def test-moons
+  [{:x -1, :y 0, :z 2}
+   {:x 2, :y -10, :z -7}
+   {:x 4, :y -8, :z 8}
+   {:x 3, :y 5, :z -1}])
+
 (defn init-moon-states [moons]
   (map (fn [m] {:coordinates m, :velocity {:x 0, :y 0, :z 0}}) moons))
 
@@ -30,19 +36,72 @@
   {:coordinates (into {} (map (fn [[k1 v1] [_ v2]] [k1 (+ v1 v2)]) coordinates velocity)),
    :velocity    velocity})
 
-(defn update-gravity [moon moons]
+(defn update-gravity [moon-state moon-states]
   (reduce (fn [result next-moon]
-            (update-velocity (assoc moon :velocity (result :velocity)) next-moon))
-          (conj moons moon)))
+            (update-velocity (assoc moon-state :velocity (result :velocity)) next-moon))
+          (cons moon-state moon-states)))
 
-; TODO:
-;  1. one round only: update-gravity for each moon with, respectively, each other moon, then update coordinates of
-;     each moon
-;  2. n rounds: do (1) for n rounds
-;  3. calculate the total energy in the system
+(defn update-moon-state [moon-state moon-states]
+  (update-coordinates (update-gravity moon-state moon-states)))
 
+(defn all-besides [l i]
+  (into (vec (drop (+ i 1) l)) (vec (reverse (vec (take i l))))))
+
+(defn update-all-moons-once [moon-states]
+  (let [four-times (take 4 (iterate inc 0))]
+    (vec (map (fn [i]
+                (update-moon-state
+                  (nth moon-states i)
+                  (all-besides (vec moon-states) i)))
+              four-times))))
+
+(defn update-all-moons [moon-states n]
+  (nth (iterate (partial update-all-moons-once) moon-states) n))
+
+(defn calculate-energy [{:keys [coordinates velocity]}]
+  (letfn [(energy [c] (reduce + (map (fn [x] (Math/abs ^Integer (val x))) c)))]
+    (* (energy coordinates) (energy velocity))))
+
+(defn calculate-total-energy [moon-states]
+  (reduce + (map calculate-energy moon-states)))
+
+
+; from here below: part 2
+
+(defn contains-val-for-key-rec [[li & ls] x n]
+  (cond
+    (nil? li) nil
+    (= li x) n
+    :else (recur ls x (+ n 1))))
+
+(defn nth-if-contained [l x]
+  (contains-val-for-key-rec l x 0))
+
+(defn does-history-repeat-itself? [[moon-states & _ :as all-moon-states] n]
+  (let [next-moon-states (update-all-moons-once moon-states)]
+    (if-let [ni (nth-if-contained all-moon-states next-moon-states)]
+      (do
+        (println n ni)
+        (+ 1 ni))
+      (recur (cons next-moon-states all-moon-states) (+ n 1)))))
+
+; Es muessen sowohl die Koordinaten als auch die Geschwindigkeiten gleich sein.
+; D.h. also man muss einen Kreis finden im System.
+; Dann bilden aber auch die jeweilige Koordinaten mit der jeweiligen Geschwindigkeit einen Kreis,
+; weil ja nur die voneinander abhängen. D.h. ich kann jetzt die Schritte n_x finden, wann sich
+; also (x, v_x) wiederholt und genauso n_y und n_z. Die Gesamtschritte bis sich das gesamte
+; System wiederholt ist dann kgV(n_x, n_y, n_z).
 
 (defn -main []
-  (let [input (slurp "resources/day12.input")]
+  (let [moons moons
+        ;moons test-moons
+
+        moon-states (init-moon-states moons)
+        ;later-moon-states (update-all-moons moon-states 2772)
+        ]
     (do
-      (println "TODO"))))
+
+      ;(println later-moon-states)
+      ;(println later-moon-states)
+      (println (does-history-repeat-itself? [moon-states] 0))
+      )))
