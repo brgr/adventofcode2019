d801149052f0f90356d3778f2fc997c3b1ef91b9
Dominik Berger <dmnkbrgr@gmail.com>
2020-01-30 09:13:17 +0100

    #day13 add and finish day13, part 1

---
+++ src/adventofcode2019/day13part1.clj
@@ -0,0 +1,27 @@
+(ns adventofcode2019.day13part1
+  [intcode]
+  (:use [adventofcode2019.core]))
+
+(defn parse-statements [[[x y tile-id] & screen-statements] block-positions]
+  (do
+    (println tile-id)
+    (let [block-positions (if (= tile-id 2)
+                            (conj block-positions [x y])
+                            (disj block-positions [x y]))]
+      (if (nil? screen-statements)
+        block-positions
+        (recur screen-statements block-positions)))))
+
+(defn parse-output [arcade-output]
+  (let [screen-statements (partition 3 arcade-output)]
+    (parse-statements screen-statements #{})))
+
+(defn -main []
+  (let [input (slurp "resources/day13.input")
+        intcode (intcode/parse-input input)
+        arcade-cabinet (intcode/init-machine intcode)
+
+        screen (parse-output ((intcode/run-machine arcade-cabinet) :output))]
+    (do
+      (println "screen:" screen)
+      (println "count:" (count screen)))))
