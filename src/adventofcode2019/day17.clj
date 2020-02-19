8556c7c4100d9a30b5056faf5cd00cfea68317ed
Dominik Berger <dmnkbrgr@gmail.com>
2020-02-19 22:00:25 +0100

    #day17 add a function that calculates the final turning sequence

--- src/adventofcode2019/day17.clj
+++ src/adventofcode2019/day17.clj
@@ -1,8 +1,6 @@
 (ns adventofcode2019.day17
   (:require [clojure.string :as str]
             [clojure.core.match :refer [match]]
-            [clojure.math.numeric-tower :as math]
-            [clojure.set :as set]
             [intcode])
   (:use [adventofcode2019.core]))
 
@@ -18,8 +16,7 @@
           (at-position area [x (- y 1)])
           (at-position area [x (+ y 1)])
           (at-position area [(+ x 1) y])
-          (at-position area [(- x 1) y])))
-  )
+          (at-position area [(- x 1) y]))))
 
 (defn intersections [area]
   (apply concat (map #(remove nil? %) (let [area (str/split area #"\n")]
@@ -32,20 +29,111 @@
                                               ;(get line x)
                                               )))))))
 
-(defn get-lines-from-intersection [area [x y]]
-  ; todo: area is currently one string; starting from [x y], find the horizontal and vertical line surrounding it
-  ; this maybe trespasses another intersection; don't just use max x/y, as there could be another line later on!
-  )
+(def area
+  (let [input (slurp "resources/day17.input")
+        intcode (intcode/parse-input input)
 
-(defn get-lines-rec [area [[x y] & intersections] lines]
-  (let [[line1 line2] (get-lines-from-intersection area [x y])
-        lines (conj lines line1 line2)]
-    (if (nil? intersections)
-      lines
-      (recur area intersections lines))))
+        machine (intcode/init-machine intcode)
+        output ((intcode/run-machine machine) :output)
+        area (->> output
+                  (map char)
+                  (apply str))]
+    area))
 
-(defn get-lines [area intersections]
-  (get-lines-rec area intersections []))
+(def area-array (str/split area #"\n"))
+
+(def intersections (intersections area))
+
+(def width 39)
+(def height 51)
+
+(defn area-at [area [x y]]
+  "This does currently not check for newline, which it gives back just as well."
+  (try
+    (let [field (get area (+ (* y (+ 1 width)) x))]
+      (if (= field \newline)
+        nil
+        field))
+    (catch Exception _ nil)))
+
+
+(def starting-point
+  (let [index (str/index-of area "^")
+        x (mod index (+ 1 width))
+        y (quot index (+ 1 width))]
+    [x y]))
+
+(def directions {:up 0 :right 1 :down 2 :left 3})
+
+(defn turn [from-direction to-direction]
+  (let [f-index (directions from-direction)
+        t-index (directions to-direction)]
+    (cond
+      (= (mod (+ f-index 1) 4) t-index) :R
+      (= (mod (- f-index 1) 4) t-index) :L)))
+
+(defn turn-left [direction]
+  (case direction
+    :up :left
+    :left :down
+    :down :right
+    :right :up))
+
+(defn turn-right [direction]
+  (case direction
+    :up :right
+    :right :down
+    :down :left
+    :left :up))
+
+(defn is-scaffold-or-bot? [area [x y]]
+  (some (partial = (area-at area [x y])) [\# \o \^ \< \> \v]))
+
+(defn move-forward-as-far-as-possible [[x y] direction]
+  (letfn [(fy [op] (op y (last (take-while
+                                 (fn [yi] (is-scaffold-or-bot? area [x (op y yi)])) (range)))))
+          (fx [op] (op x (last (take-while
+                                 (fn [xi] (is-scaffold-or-bot? area [(op x xi) y])) (range)))))]
+    (let [[[x y] steps] (case direction
+                          :up (let [min-y (fy -)]
+                                [[x min-y] (- y min-y)])
+                          :left (let [min-x (fx -)]
+                                  [[min-x y] (- x min-x)])
+                          :down (let [max-y (fy +)]
+                                  [[x max-y] (- max-y y)])
+                          :right (let [max-x (fx +)]
+                                   [[max-x y] (- max-x x)]))]
+      (if (zero? steps) nil [[x y] steps]))))
+
+(defn left-from-position [position direction]
+  (let [new-direction (turn-left direction)]
+    (if-let [[new-position steps] (move-forward-as-far-as-possible position new-direction)]
+      [new-position new-direction steps])))
+
+(defn right-from-position [position direction]
+  (let [new-direction (turn-right direction)]
+    (if-let [[new-position steps] (move-forward-as-far-as-possible position new-direction)]
+      [new-position new-direction steps])))
+
+(defn next [position direction]
+  (if-let [result (left-from-position position direction)]
+    result
+    (if-let [result (right-from-position position direction)]
+      result)))
+
+(defn get-turning-sequence [starting-point]
+  (loop [current-position starting-point
+         direction :up
+         movements []]
+    (let [[new-position new-direction steps-moved :as next] (next current-position direction)]
+      (if (nil? next)
+        movements
+        (recur new-position
+               new-direction
+               (conj movements [(turn direction new-direction) steps-moved]))))))
+
+(get-turning-sequence starting-point)
+
 
 (defn -main []
   (let [input (slurp "resources/day17.input")
